3fe4ac2c1b20944356f69fd04b2ef009301e28de
SewonK <cutecat1128@naver.com>
2022-07-18 11:40:16 +0900

    [Feat] Post 엔티티 생성

---
+++ src/main/java/sewon/blog/domain/Post.java
@@ -0,0 +1,32 @@
+package sewon.blog.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "post")
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String author;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
